06436b26ff412d3d02e82820b9f5b06b4f658a7b
Guillermo Moraleda <guille.moraleda@gmail.com>
2025-07-08 16:11:30 +0200

    fix: removed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
+  define: {},
 });
